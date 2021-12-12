686d5dabf4b1fb24e1272dfea222749415f69800
Gafar Bagas <gafarbagas@gmail.com>
2021-12-12 18:22:15 +0700

    fixing all

--- logout.php
+++ logout.php
@@ -1,4 +1,7 @@
 <?php
+	session_start();
+    $koneksi = mysqli_connect("localhost","root","","db_absensi");
+	
 	session_destroy();
 	echo "<script>alert('Anda telah logout');</script>";
 	echo "<script>location='login.php';</script>";
